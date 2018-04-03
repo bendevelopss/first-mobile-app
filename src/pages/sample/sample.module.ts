54be26a8639d6176bcaac894de1392405463df74
Jaja Pabatao <jajapabatao@gmail.com>
2018-04-03 17:14:50 +0800

    firstapp

---
+++ src/pages/sample/sample.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SamplePage } from './sample';
+
+@NgModule({
+  declarations: [
+    SamplePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SamplePage),
+  ],
+  providers: [
+    
+  ],
+})
+export class SamplePageModule {}
+
+
